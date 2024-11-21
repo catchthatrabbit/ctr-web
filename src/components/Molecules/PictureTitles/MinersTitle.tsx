e7e3ca9135f7eeb7ad92eaa02cecd97d86410a1b
Ingrid Novak <ingridnovak240@gmail.com>
2024-11-21 18:02:01 +0000

    added search, network difficulty value, changed styles for header, table

--- src/components/Molecules/PictureTitles/MinersTitle.tsx
+++ src/components/Molecules/PictureTitles/MinersTitle.tsx
@@ -2,7 +2,13 @@ import PictureTitle from "@site/src/components/Atoms/PictureTitle/PictureTitle";
 import { HeadAbove } from "@site/src/icons";
 
 const MinersTitle = () => {
-  return <PictureTitle title="Miners" image={<HeadAbove />} />;
+  return (
+    <PictureTitle
+      title="Miners"
+      titleStyles={{ marginTop: "15px" }}
+      image={<HeadAbove />}
+    />
+  );
 };
 
 export default MinersTitle;
