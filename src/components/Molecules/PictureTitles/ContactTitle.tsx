345384ba25edeffefc53e9c61060533929a47577
Ingrid Novak <ingridnovak240@gmail.com>
2025-01-14 13:02:53 +0000

    added styles for contact page

--- src/components/Molecules/PictureTitles/ContactTitle.tsx
+++ src/components/Molecules/PictureTitles/ContactTitle.tsx
@@ -1,8 +1,18 @@
 import PictureTitle from "@site/src/components/Atoms/PictureTitle/PictureTitle";
 import { Ears } from "@site/src/icons";
+import useMediaQueries from "@site/src/hooks/useMediaQueries/useMediaQueries";
 
 const ContactTitle = () => {
-  return <PictureTitle title="Contact" image={<Ears />} />;
+  const { mobile } = useMediaQueries();
+  const titleStyles = {
+    fontWeight: "800",
+    fontFamily: "Exo",
+
+    ...(mobile && { fontSize: "32px", marginTop: "8px" }),
+  };
+  return (
+    <PictureTitle title="Contact" image={<Ears />} titleStyles={titleStyles} />
+  );
 };
 
 export default ContactTitle;
--- src/components/Pages/Contact/controls.ts
+++ src/components/Pages/Contact/controls.ts
@@ -1,11 +1,16 @@
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useMapChartData from "../Dashboard/hooks/useMapChartData";
 
 interface CustomFields {
   MAINTAINERS_SUPPORT_EMAIL?: string | (string | { [email: string]: string })[];
   MAINTAINERS_SUPPORT_DESCRIPTION?: string;
-  MAINTAINERS_SECURITY_EMAIL?: string | (string | { [email: string]: string })[];
+  MAINTAINERS_SECURITY_EMAIL?:
+    | string
+    | (string | { [email: string]: string })[];
   MAINTAINERS_SECURITY_DESCRIPTION?: string;
-  MAINTAINERS_COMMERCIAL_EMAIL?: string | (string | { [email: string]: string })[];
+  MAINTAINERS_COMMERCIAL_EMAIL?:
+    | string
+    | (string | { [email: string]: string })[];
   MAINTAINERS_COMMERCIAL_DESCRIPTION?: string;
 }
 
@@ -15,11 +20,22 @@ const useControls = () => {
   const customFields = siteConfig.customFields as Partial<CustomFields>;
 
   const maintainersSupportEmail = customFields.MAINTAINERS_SUPPORT_EMAIL ?? [];
-  const maintainersSupportDescription = customFields.MAINTAINERS_SUPPORT_DESCRIPTION ?? "";
-  const maintainersSecurityEmail = customFields.MAINTAINERS_SECURITY_EMAIL ?? [];
-  const maintainersSecurityDescription = customFields.MAINTAINERS_SECURITY_DESCRIPTION ?? "";
-  const maintainersCommercialEmail = customFields.MAINTAINERS_COMMERCIAL_EMAIL ?? [];
-  const maintainersCommercialDescription = customFields.MAINTAINERS_COMMERCIAL_DESCRIPTION ?? "";
+  const maintainersSupportDescription =
+    customFields.MAINTAINERS_SUPPORT_DESCRIPTION ?? "";
+  const maintainersSecurityEmail =
+    customFields.MAINTAINERS_SECURITY_EMAIL ?? [];
+  const maintainersSecurityDescription =
+    customFields.MAINTAINERS_SECURITY_DESCRIPTION ?? "";
+  const maintainersCommercialEmail =
+    customFields.MAINTAINERS_COMMERCIAL_EMAIL ?? [];
+  const maintainersCommercialDescription =
+    customFields.MAINTAINERS_COMMERCIAL_DESCRIPTION ?? "";
+
+  const {
+    infoBoxItems: infoBoxMapData,
+    poolFee,
+    isLoading: isLoadingMapChart,
+  } = useMapChartData();
 
   return {
     maintainersCommercialDescription,
@@ -28,6 +44,8 @@ const useControls = () => {
     maintainersSecurityEmail,
     maintainersSupportDescription,
     maintainersSupportEmail,
+    infoBoxMapData,
+    isLoadingMapChart,
   };
 };
 
